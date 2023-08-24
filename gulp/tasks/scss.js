f83d4dbd66b61d657aa9a6914baa497d827ee62c
Александр Ильин <aleksandr-ilin@integlab.ru>
2023-08-24 10:53:41 +0500

    SCSS task update

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -9,17 +9,45 @@ import groupCssMediaQueries from 'gulp-group-css-media-queries'; // Группи
 import path from 'path';
 import * as glob from 'glob';
 const sass = gulpSass(dartSass);
+import fs from 'fs';
+let isFirstBuild = true;
 
 export const scss = () => {
+  console.log('Первичный запуск SCSS компиляции:', isFirstBuild);
+
   const pageFolders = glob.sync(`src/styles/scss/pages/*/`);
+
   const mainFiles = pageFolders.map((folder) => {
     const folderName = path.basename(folder);
     return `${folder}/${folderName}.scss`;
   });
 
+  const latestModifiedFile = mainFiles.reduce((latestFile, currentFile) => {
+    const currentModifiedTime = fs.statSync(currentFile).mtime;
+    if (!latestFile || currentModifiedTime > latestFile.modifiedTime) {
+      return {
+        filePath: currentFile,
+        modifiedTime: currentModifiedTime,
+      };
+    }
+    return latestFile;
+  }, null);
+
+  let filesToCompile = mainFiles;
+
+  if (!isFirstBuild) {
+    // Если не первый запуск, компилировать только последний измененный файл
+    filesToCompile = [latestModifiedFile.filePath];
+  }
+
+  isFirstBuild = false;
+
+  // console.log(mainFiles, 'файлы');
+  // console.log(latestModifiedFile, 'Последний измененный файл');
+
   return (
     app.gulp
-      .src(mainFiles, { sourcemaps: app.isBuildDefault })
+      .src(filesToCompile, { sourcemaps: app.isBuildDefault })
       .pipe(
         app.plugins.plumber(
           app.plugins.notify.onError({
@@ -55,14 +83,14 @@ export const scss = () => {
             app.isBuildMin ||
             app.isBuildOptimized,
           autoPrefixer({
-            grid: true,
-            overrideBrowserslist: ['last 3 versions'],
+            grid: false,
+            overrideBrowserslist: ['last 10 versions'],
             cascade: true,
           })
         )
       )
       // Расскомментировать если нужен обычный дубль файла стилей
-      .pipe(app.gulp.dest(app.path.build.css))
+      // .pipe(app.gulp.dest(app.path.build.css))
       .pipe(
         app.plugins.if(
           app.isBuildDefault ||
